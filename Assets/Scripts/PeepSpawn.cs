7965ba881ef252a12a839c10ead6c91079785499
JamesBachl <BachlerJames@yahoo.com>
2025-02-22 21:02:12 -0700

    Peep & Wafer

--- Assets/Scripts/PeepSpawn.cs
+++ Assets/Scripts/PeepSpawn.cs
@@ -1,10 +1,12 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PeepSpawn : MonoBehaviour
 {
     [SerializeField] private GameObject Spawner, Peep;
     [SerializeField] private float SpawnSpeed;
+    [SerializeField] private List<GameObject> Cheeps;
     float X;
     float Y;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,7 +21,8 @@ public class PeepSpawn : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(SpawnSpeed);
-            Instantiate(Peep, new Vector3(X, Y, 0), Quaternion.identity);
+            GameObject choice = Cheeps[Random.Range(0, Cheeps.Count)];
+            Instantiate(choice, new Vector3(X, Y, 0), Quaternion.identity);
         }
     }
 }
